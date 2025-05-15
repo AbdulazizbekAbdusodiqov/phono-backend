4980f23e4a3a39b7fb75e403655979187ea59c15
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-05-15 11:30:59 +0500

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
     PrismaModule,
     AuthModule,
-    AdminModule
+    AdminModule,
+    ProductModule
   ],
   controllers: [],
   providers: [],
