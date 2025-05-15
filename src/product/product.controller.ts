2b05f882fb26b1479c8f42cd320d659c71773940
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-05-15 13:45:41 +0500

    add product flter

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,32 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProductService } from './product.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { ProductService } from './product.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) { }
 
+  @ApiOperation({ summary: "Yangi product qo'shish" })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  @ApiOperation({ summary: "productlarni ko'rish" })
   @Get()
   findAll() {
     return this.productService.findAll();
   }
 
+  @ApiOperation({ summary: "productni ko'rish" })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id);
   }
 
+  @ApiOperation({ summary: "userning productlarini ko'rish" })
+  @Get('user/:id')
+  getProductByUserId(@Param('id') id: string) {
+    return this.productService.getProductByUserId(+id);
+  }
+
+  @ApiOperation({ summary: "productlarni title bo'yicha qidirish (query param bilan)" })
+  @Get('search')  
+  getProductByTitleQuery(@Query('search') search: string) {
+    return this.productService.getProductByTitleQuery(search);
+  }
+
+  @ApiOperation({ summary: "pending productlarni ko'rish" })
+  @UseGuards(AdminGuard)
+  @Get('pending')
+  getPendingProducts() {
+    return this.productService.getPendingProducts();
+  }
+
+  @ApiOperation({ summary: "productni approved qilish" })
+  @UseGuards(AdminGuard)
+  @Get('approved/:id')
+  approveProduct(@Param('id') id: string) {
+    return this.productService.approvProduct(+id);
+  }
+
+  @ApiOperation({ summary: "productni rejected qilish" })
+  @UseGuards(AdminGuard)
+  @Get('rejected/:id')
+  rejectProduct(@Param('id') id: string) {
+    return this.productService.rejectProduct(+id);
+  }
+
+  @ApiOperation({ summary: "productni yangilash" })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto);
   }
 
+  @ApiOperation({ summary: "productni o'chirish" })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productService.remove(+id);
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { isChecked } from '@prisma/client';
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
   create(createProductDto: CreateProductDto) {
     return this.prisma.product.create({
       data: createProductDto,
@@ -13,21 +14,102 @@ export class ProductService {
   }
 
   findAll() {
-    return this.prisma.product.findMany();
+    return this.prisma.product.findMany({
+      where: { is_deleted: false },
+      include: {
+        user: true,
+        brand: true,
+        model: true,
+        color: true,
+        currency: true,
+        address: true,
+        product_image: true,
+      },
+    });
+  }
+
+  getProductByUserId(userId: number) {
+    return this.prisma.product.findMany({
+      where: { user_id: userId, is_checked: isChecked.APPROVED, is_deleted: false },
+      include: {
+        user: true,
+        brand: true,
+        model: true,
+        color: true,
+        currency: true,
+        address: true,
+        product_image: true,
+      },
+    });
+  }
+
+  getPendingProducts() {
+    return this.prisma.product.findMany({
+      where: { is_checked: isChecked.PENDING, is_deleted: false },
+      include: {
+        user: true,
+        brand: true,
+        model: true,
+        color: true,
+        currency: true,
+        address: true,
+        product_image: true,
+      },
+    });
+  }
+
+  approvProduct(id: number) {
+    return this.prisma.product.update({
+      where: { id, is_deleted: false },
+      data: { is_checked: isChecked.APPROVED },
+    });
+  }
+
+  rejectProduct(id: number) {
+    return this.prisma.product.update({
+      where: { id, is_deleted: false },
+      data: { is_checked: isChecked.REJECTED },
+    });
   }
 
   findOne(id: number) {
-    return this.prisma.product.findUnique({ where: { id } });
+    return this.prisma.product.findUnique({
+      where: { id, is_deleted: false },
+      include: {
+        user: true,
+        brand: true,
+        model: true,
+        color: true,
+        currency: true,
+        address: true,
+        product_image: true
+      }
+    });
+  }
+
+  getProductByTitleQuery(query: string) {
+    return this.prisma.product.findMany({
+      where: { title: { contains: query, mode: 'insensitive' }, is_deleted: false },
+      include: {
+        user: true,
+        brand: true,
+        model: true,
+        color: true,
+        currency: true,
+        address: true,
+        product_image: true,
+      },
+    });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return this.prisma.product.update({
-      where: { id },
+      where: { id, is_deleted: false },
       data: updateProductDto,
     });
   }
 
   remove(id: number) {
-    return this.prisma.product.delete({ where: { id } });
+    return this.prisma.product.update({ where: { id, is_deleted: false }, data: { is_deleted: true } });
   }
 }
