0f236dce3fdfb7f0ce33171916ed6b851bec39ee
Husnidin <husnidin@Husnidins-MacBook-Air.local>
2025-05-30 13:16:32 +0500

    fix:brand_image_upload

--- src/brand/dto/create-brand.dto.ts
+++ src/brand/dto/create-brand.dto.ts
@@ -1,12 +1,12 @@
-import { IsString, Length } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateBrandDto {
   @ApiProperty({
-    description: 'Brand nomi',
-    example: 'Nike',
+    description: "Brand nomi",
+    example: "Nike",
   })
-  @IsString({ message: 'Name must be a string' })
-  @Length(2, 50, { message: 'Name must be between 2 and 50 characters' })
+  @IsString({ message: "Name must be a string" })
+  @Length(2, 50, { message: "Name must be between 2 and 50 characters" })
   name: string;
 }
