617540124f91f429bdb47203f6c73b2a8a9bba9c
SamandarOydinov <samandaroydinov0001@gmail.com>
2025-06-09 17:18:14 +0500

    send code to email

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,34 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
-import { Admin } from '@prisma/client';
+import { Admin, Email } from '@prisma/client';
 
 @Injectable()
 export class MailService {
+  constructor(private mailerService: MailerService) {}
 
-    constructor(private mailerService: MailerService) { }
+  // async sendMail(user: User) {
+  //     const url = `${process.env.API_URL}/api/auth/activate/${user.activation_link}`;
+  //     await this.mailerService.sendMail({
+  //         to: user.email,
+  //         subject: `PHONO-TECHga hush kelibsiz!`,
+  //         template: "./confirm",
+  //         context: {
+  //             name: `${user.first_name} ${user.last_name}`,
+  //             url
+  //         },
+  //     });
+  // }
+  async sendAdminMail(admin: Admin) {
+    const url = `${process.env.API_URL}/api/auth/admin/activate/${admin.activation_link}`;
+    await this.mailerService.sendMail({
+      to: admin.email,
+      subject: `PHONO-TECH ga hush kelibsiz!`,
+      template: './confirm',
+      context: {
+        name: `${admin.first_name} ${admin.last_name}`,
+        url,
+      },
+    });
+  }
 
-    // async sendMail(user: User) {
-    //     const url = `${process.env.API_URL}/api/auth/activate/${user.activation_link}`;
-    //     await this.mailerService.sendMail({
-    //         to: user.email,
-    //         subject: `PHONO-TECHga hush kelibsiz!`,
-    //         template: "./confirm",
-    //         context: {
-    //             name: `${user.first_name} ${user.last_name}`,
-    //             url
-    //         },
-    //     });
-    // }
-    async sendAdminMail(admin: Admin) {
-        const url = `${process.env.API_URL}/api/auth/admin/activate/${admin.activation_link}`;
-        await this.mailerService.sendMail({
-            to: admin.email,
-            subject: `PHONO-TECH ga hush kelibsiz!`,
-            template: "./confirm",
-            context: {
-                name: `${admin.first_name} ${admin.last_name}`,
-                url
-            },
-        });
-    }
+  // email.service.ts
+  async sendEmailActivationLink(email: Email) {
+    const url = `${process.env.API_URL}/api/email/activate/${email.activation_link}`;
+    await this.mailerService.sendMail({
+      to: email.email,
+      subject: `PHONO-TECH: Email manzilingizni tasdiqlang`,
+      template: './confirm-email', // .hbs fayl bo'lishi kerak
+      context: {
+        email: email.email,
+        url,
+      },
+    });
+  }
 }
