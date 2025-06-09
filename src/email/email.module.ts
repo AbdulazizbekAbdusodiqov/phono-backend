b46d841e861a3ce359d7fd7e292318e98ae379ea
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-06-09 17:34:43 +0500

    ok

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MailModule],
   controllers: [EmailController],
   providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
